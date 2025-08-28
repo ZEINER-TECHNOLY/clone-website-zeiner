68f142575da114070afbd1fd79eab5543e8eb8bc
Quimbuadi <antonioquimbuadi@gmail.com>
2025-08-28 23:55:04 +0000

    finalizando o header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 //configurando o navbar para mobile
 const menuToggle = document.getElementById('menu-toggle')
 const mobileMenu = document.getElementById('mobile-menu')
+const closeIcon = document.getElementById('icon-close')
+const openIcon = document.getElementById('icon-open')
 
 menuToggle.addEventListener('click', (event) => {
-    mobileMenu.classList.toggle('hidden')
+    
+    const isHidden = mobileMenu.classList.toggle('hidden')
+    if(isHidden){
+        openIcon.classList.remove('hidden')
+        closeIcon.classList.add('hidden')
+    }else{
+        openIcon.classList.add('hidden')
+        closeIcon.classList.remove('hidden')
+    }
+
 })
 
