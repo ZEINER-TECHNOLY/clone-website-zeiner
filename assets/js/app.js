4b4eb486c05b30ff81319e0ce600a1d6d36d853a
quimbuadi <antonioquimbuadi@gmail.com>
2025-09-01 12:38:16 +0100

    adicionando animação nos cards dos cursos

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,36 @@ const closeIcon = document.getElementById('icon-close')
 const openIcon = document.getElementById('icon-open')
 
 menuToggle.addEventListener('click', (event) => {
-    
+
     const isHidden = mobileMenu.classList.toggle('hidden')
-    if(isHidden){
+    if (isHidden) {
         openIcon.classList.remove('hidden')
         closeIcon.classList.add('hidden')
-    }else{
+    } else {
         openIcon.classList.add('hidden')
         closeIcon.classList.remove('hidden')
     }
 
 })
+
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const carousel = document.getElementById('carousel');
+let currentIndex = 0;
+const totalSlides = carousel.children.length;
+
+function goToSlide(index) {
+    const offset = -index * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    goToSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    goToSlide(currentIndex);
+});
 
