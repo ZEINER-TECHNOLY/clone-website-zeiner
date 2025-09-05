221d466856d9fef2bc84605a4998fbf325f5fe0a
quimbuadi <antonioquimbuadi@gmail.com>
2025-09-05 16:14:33 +0100

    estilizando menu mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,7 @@
-function toggleCard(button) {
-    const content = button.nextElementSibling;
-    const icon = button.querySelector('svg');
+const toggleBtn = document.getElementById('toggle-btn')
+const toggleNav = document.getElementById('nav-toggle')
 
-    if (content.classList.contains('max-h-0')) {
-      content.classList.remove('max-h-0');
-      content.classList.add('max-h-[300px]');
-      icon.classList.add('rotate-180');
-    } else {
-      content.classList.add('max-h-0');
-      content.classList.remove('max-h-[300px]');
-      icon.classList.remove('rotate-180');
-    }
-  }
+toggleBtn.addEventListener('click', () => {
+    console.log('ola mundo')
+    toggleNav.classList.toggle('hidden')
+})
