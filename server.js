452a43693c833923d487909a445593118b51997b
quimbuadi <antonioquimbuadi@gmail.com>
2025-09-09 16:12:55 +0100

    refatoração

--- server.js
+++ server.js
@@ -7,9 +7,27 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+//rota index
 app.get('/', (req, res) => {
     res.render('index', {nome: 'Quimbuadi'})
 })
+//rota login
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+//rota cadastro
+app.get('/criar-conta', (req, res) => {
+    res.render('cadastro')
+})
+
+//rota softwares
+app.get('/softwares', (req, res) => {
+    res.render('softwares')
+})
+
+//rota 
 
 app.listen(port, () => {
     console.log(`Servidor rodando no endereço: http://localhost:${port}`)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-// tailwind.config.js
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,php}"], // Ajuste para seus arquivos
-  theme: {
-    extend: {
-      colors: {
-        primary: "#FF0000",   // Vermelho
-        dark: "#000000",      // Preto
-        cinza: "#808080",   // Cinza
-        verde: "#008000",    // Verde
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
       },
     },
-  },
-  plugins: [],
-}
+  }
